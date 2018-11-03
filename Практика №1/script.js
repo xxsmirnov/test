99d98702e2ca225ec26c89b6eedc6f8e78fcf763
Dima <aua008@gmail.com>
2018-11-03 17:31:24 +0200

    Это наш первый коммит

---
+++ Практика №1/script.js
@@ -0,0 +1,25 @@
+var persone = {
+    name: "john",
+    age: 25,
+    isMarried: false
+};
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'grnd.jpg', 345];
+
+console.log(arr[0]);
+
+// alert('Hello world!');
+
+// let answer = confirm('tulull');
+
+// console.log(answer);
+
+// let answer = prompt('Есть ли вам 18?', 'Да');
+// console.log(typeof(answer));
+
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
